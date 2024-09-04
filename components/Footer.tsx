d8e61cfb1713f73f36214144f8e28282fdc22872
RoronoaZoroDAce <90967650+RoronoaZoroDAce@users.noreply.github.com>
2024-09-04 18:39:24 +0530

    implement initial file and folder structure

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <nav className="bordere-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">Home</Link>
+    </nav>
+  );
+};
+
+export default Footer;
